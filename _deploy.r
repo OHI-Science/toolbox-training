f4f017ff8d2099aa9f0dda03223b945477a778bd
jules32 <jstewart@nceas.ucsb.edu>
2017-09-14 11:40:34 -0700

    rebuild and push

--- _deploy.r
+++ _deploy.r
@@ -7,21 +7,23 @@
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
 ## read in github token outside of repo, generated with url above 
-gh_token <- scan('~/.github-token', 'character', quiet = T)
+gh_token <- scan('~/.github-token', 'character', quiet = TRUE)
 Sys.setenv(GH_TOKEN=gh_token)
 
 ## book variables
 repo_name = 'toolbox-training'
-repo_slug = sprintf('OHI-Science/toolbox-training')
 repo_slug = sprintf('OHI-Science/%s', repo_name)
+repo_url <- sprintf('https://github.com/%s', repo_slug)
 dir_gh_pages = sprintf('~/github/clip-n-ship/%s', repo_name)
 dir_master = sprintf('~/github/%s', repo_name)
 dir_master_book = sprintf('~/github/%s/_book', repo_name)
 
 
 ## clone gh-pages branch to temporary location
-unlink(dir_gh_pages, recursive=T, force=T) # delete if temporary location exists
-repo = git2r::clone(url=repo_url, local_path=normalizePath(dir_gh_pages, mustWork=F), branch='gh-pages')
+unlink(dir_gh_pages, recursive=TRUE, force=TRUE) # delete if temporary location exists
+repo = git2r::clone(url=repo_url, 
+                    local_path=normalizePath(dir_gh_pages, mustWork=FALSE), 
+                    branch='gh-pages')
 
 ## copy master branch _book/*.html files to gh-pages
 system(sprintf('cp %s/* %s', dir_master_book, dir_gh_pages))
